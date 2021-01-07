53b6252628e59849a88a51e37a9af3634b2b148c
Emily <>
2021-01-07 00:45:59 -0500

    node sets

--- Node.java
+++ Node.java
@@ -16,4 +16,16 @@ public class Node{
   public Node getPrev(){
     return prev;
   }
+
+  public void setData(String data){
+    this.data = data;
+  }
+
+  public void setNext(Node next){
+    this.next = next;
+  }
+
+  public void setPrev(Node prev){
+    this.prev = prev;
+  }
 }
